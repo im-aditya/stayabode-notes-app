9294435630c66015079986d9b1e86e0aee372c2a
Aditya <im.aditya0689@gmail.com>
2017-11-27 12:27:15 +0530

    Initial commit

---
+++ app/src/main/java/com/stayabode/notes/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.stayabode.notes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                
+            }
+        });
+    }
+}
